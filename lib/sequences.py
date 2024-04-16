e41b6d398ded0200ca0c205b57813bfbe6759ec8
Genesis McGruder <genesismcgruder@gmail.com>
2024-04-16 19:46:44 -0400

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1
+    fib_seq = []
+
+    for _ in range(length):
+        fib_seq.append(a)
+        a, b = b, a + b
+
+    print(fib_seq)
